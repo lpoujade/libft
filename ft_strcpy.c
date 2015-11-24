12621dbce1252b384c27f1bcb0ca5b7b3d24aa8f
lpoujade <lpoujade@student.42.fr>
2015-11-24 11:09:19 +0100

    to continue (strcpy)

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,42 +6,29 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 21:21:24 by lpoujade          #+#    #+#             */
-/*   Updated: 2015/11/23 21:23:27 by lpoujade         ###   ########.fr       */
+/*   Updated: 2015/11/24 10:43:17 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-char	ft_strcpy(char *dest, const char *src)
-{
-if (*dest && *src)
-{
-	int src_len;
-	int c;
-
-	c = 0;
-	src_len = ft_strlen(src);
-	while (c <= src_len)
-	{
-		ft_putstr("entree, c=");ft_putnbr(c);ft_putchar('\n');
-		dest[c] = src[c];
-		c++;
-	}
-
-	return (*dest);
-}
-return (-1);
-}
-*/
-
 char	ft_strcpy(char *dest, const char *src)
 {
 	if (*dest && *src)
 	{
-		*dest = *src;
-		ft_strcpy(dest++, src++);
+		int src_len;
+		int c;
+
+		c = 0;
+		src_len = ft_strlen(src);
+		while (c <= src_len)
+		{
+			ft_putstr("entree, c=");ft_putnbr(c);
+			dest[c] = src[c];
+			c++;
+		}
+
+		return (*dest);
 	}
-	*dest = '\0';
-	return (*dest);
+	return (-1);
 }
