c32d91845e84e9644868adc6a1dc3c3474d81b41
lpoujade <lpoujade@student.42.fr>
2015-11-24 14:13:20 +0100

    add some fonctions defs

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 21:45:10 by lpoujade          #+#    #+#             */
-/*   Updated: 2015/11/24 10:04:04 by lpoujade         ###   ########.fr       */
+/*   Updated: 2015/11/24 14:08:40 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <string.h>
 
 int		ft_strlen(const char *str);
-char	ft_strcpy(char *dest, const char *src);
+char	*ft_strcpy(char *dest, const char *src);
 int		ft_strcmp(char *s1, char *s2);
 
 void	ft_putchar(char c);
@@ -38,28 +38,28 @@ void	 ft_memdel(void **ap);
 **
 ** classiques 
 **
-**strdup
-**strncpy
-**strcat
-**strncat
-**strlcat
-**strchr
-**strrchr
-**strstr
-**strnstr
-**strncmp
-**memset
-**bzero
-**memcpy
-**memccpy
-**memmove
-**memchr
-**memcmp
-**atoi
-**isalpha
-**isdigit
-**isalnum
-**isascii
+** char *ft_strdup(const char *s1); --> ft_memalloc(sizeof(s1)) && ft_strcpy && return *p
+** char *ft_strncpy(char *dest, const char *src, size_t n); --> copy almost n caract, else fill with \0
+** char *ft_strcat(char *s1, const char *s2); --> if s1 >= s1+s2, s1.append(s2).append(\0)
+** char *ft_strncat(char *s1, const char *s2); --> if s1 >= s1+s2, s1.append(s2).append(\0) | n caract MAX
+** size_t ft_strlcat(char *dest, char const *src, size_t size);  --> dest.append(src) + \0 "at most size - len(dest) - 1 bytes" 
+** char *ft_strchr(char const *s, int c); --> locate first occurence of c ( converted to char ) in s. '\0' possible
+** char *ft_strrchr(char const *s, int c); --> like strchr, but last occurence instead of first.
+** char *ft_strstr(char const *s1, char const *s2); --> locate 1st occurence of s2 in s1
+** char *ft_strnstr(char const *s1, char const *s2, size_t n); --> strstr but not more n caract are searched ( n caract from s2 ? )
+** int ft_strncmp(char const *s1, char const *s2, size_t n); --> compare not more than n caract
+** void *ft_memset(void *b, int c, size_t len); --> write len*c in *b ( return *b)
+** void ft_bzero(void *s, size_t n); --> write n*\0 in s
+** void *ft_memcpy(void *dest, void const *src, size_t n); --> cp n bytes from src to dest
+** void *ft_memccpy(void *dest, void const *src, int c, size_t n); --> cp n bytes from src to dest else if c (converted to unsigned char) in src, so point
+** void *ft_move(void *dest, void const *src, size_t len); --> cp len bytes from src to dest
+** void *ft_memchr(void const *s, int c, size_t n); --> locates c (unsigned char) in sm return pointer to it
+** void *ft_memcmp(void const *s1, void const *s2, size_t n); --> compare s1, s2 : len(both)=n
+** int ft_atoi(char const *str); --> convert str.... to int representation (???)
+** int ft_isalpha(int c); --> alpha: maj or min
+** int ft_isdigit(int c); --> depend on locale. else : 0->9
+** int ft_isalnum(int c); --> isalpha()r isdigit()
+** int ft_isascii(int c); --> is ascii
 **isprint
 **toupper
 **tolower
