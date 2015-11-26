f8b1f2395aaf1eb5cb179a8b7a74e8a54eb1e186
lpoujade <lpoujade@student.42.fr>
2015-11-26 10:24:45 +0100

    memalloc use now bzero

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,16 +6,19 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 21:59:31 by lpoujade          #+#    #+#             */
-/*   Updated: 2015/11/24 10:12:59 by lpoujade         ###   ########.fr       */
+/*   Updated: 2015/11/26 10:09:03 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 void	*ft_memalloc(size_t size)
 {
 	int *p;
 
 	p = malloc(size);
+	if (p)
+		ft_bzero(p)
 	return (p);
 }
