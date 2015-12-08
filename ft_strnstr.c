e345e6f4714ccef249191bc9a70eaf60437cb615
lpoujade <lpoujade@student.42.fr>
2015-12-08 18:58:08 +0100

    working strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 15:54:22 by lpoujade          #+#    #+#             */
-/*   Updated: 2015/12/08 16:04:14 by lpoujade         ###   ########.fr       */
+/*   Updated: 2015/12/08 18:55:27 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 char	*ft_strnstr(char const *s1, char const *s2, size_t n)
 {
 	size_t	i;
-	size_t	i1;
+	size_t	j;
 
 	if (!*s2 || !s2)
 		return ((char *)s1);
-	i1 = 0;
-	while (s1[i1] && i < n)
+	j = 0;
+	while (s1[j] && j < n)
 	{
 		i = 0;
-		while (s1[i1 + i] == s2[i] && (i + i1) < n)
+		while (s1[j + i] == s2[i] && (i + j) < n)
 		{
-			if (!s2[i])
-				return ((char *)&s1[i1]);
 			i++;
+			if (!s2[i])
+				return ((char *)&s1[j]);
 		}
-		i1++;
+		j++;
 	}
 	return (NULL);
 }
