50cba43e7f8caf5b14c660e5338f21d1e8ba5f70
lpoujade <lpoujade@student.42.fr>
2015-12-18 14:29:53 +0100

    removed mem alloc in memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,20 +6,23 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 23:11:59 by lpoujade          #+#    #+#             */
-/*   Updated: 2015/12/12 11:51:34 by lpoujade         ###   ########.fr       */
+/*   Updated: 2015/12/18 14:27:18 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, void const *src, size_t len)
+void	*ft_memmove(void *dst, void const *src, size_t len)
 {
-	void *tmp;
+	unsigned char	*t_src;
+	unsigned char	*t_dst;
 
-	tmp = ft_memalloc(len);
-	if (!ft_memcpy(tmp, src, len))
-		return (NULL);
-	ft_memcpy(dest, tmp, len);
-	ft_memdel(&tmp);
-	return (dest);
+	t_src = (unsigned char *)src;
+	t_dst = (unsigned char *)dst;
+	if (t_src > (t_dst + len))
+		ft_memcpy(t_src, t_dst, len);
+	else
+		while (len--)
+			t_src[len] = t_dst[len];
+	return (dst);
 }
