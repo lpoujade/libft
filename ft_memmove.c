3bcb1fd0847a5d8521bc920b9e9fceda91c96141
lpoujade <lpoujade@student.42.fr>
2015-12-18 14:47:05 +0100

    ok

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 23:11:59 by lpoujade          #+#    #+#             */
-/*   Updated: 2015/12/18 14:27:18 by lpoujade         ###   ########.fr       */
+/*   Updated: 2015/12/18 14:46:15 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,19 @@ void	*ft_memmove(void *dst, void const *src, size_t len)
 {
 	unsigned char	*t_src;
 	unsigned char	*t_dst;
+	size_t			i;
 
+	i = 0;
 	t_src = (unsigned char *)src;
 	t_dst = (unsigned char *)dst;
-	if (t_src > (t_dst + len))
-		ft_memcpy(t_src, t_dst, len);
-	else
+	if (t_src < t_dst)
 		while (len--)
-			t_src[len] = t_dst[len];
+			t_dst[len] = t_src[len];
+	else
+		while (i < len)
+		{
+			t_dst[i] = t_src[i];
+			i++;
+		}
 	return (dst);
 }
