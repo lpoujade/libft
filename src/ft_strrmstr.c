485845fb17ac6e1ae53f92a1148ce4c1ca5ca7e9
lpoujade <lpoujade@student.42.fr>
2016-09-22 13:42:18 +0200

    strrmstr now does'nt alloc

--- src/ft_strrmstr.c
+++ src/ft_strrmstr.c
@@ -6,7 +6,7 @@
 /*   By: lpoujade <lpoujade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/20 13:55:56 by lpoujade          #+#    #+#             */
-/*   Updated: 2016/09/20 14:29:54 by lpoujade         ###   ########.fr       */
+/*   Updated: 2016/09/22 12:24:54 by lpoujade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*ft_strrmstr(char *str, char *torm)
 {
-	char	*ret;
 	char	*subs;
 	int		c;
 	int		sec;
@@ -23,14 +22,13 @@ char	*ft_strrmstr(char *str, char *torm)
 	sec = 0;
 	if (!(subs = ft_strstr(str, torm)))
 		return (str);
-	if (!(ret = ft_strnew(ft_strlen(str) - ft_strlen(torm))))
-		return (NULL);
 	while (str[c])
 	{
 		if ((str + c) == subs)
 			sec += ft_strlen(torm);
-		ret[c] = str[c + sec];
+		str[c] = str[c + sec];
 		c++;
 	}
-	return (ret);
+	str[c] = 0;
+	return (str);
 }
